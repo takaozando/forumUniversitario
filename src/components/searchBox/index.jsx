6a810f7e63b37ea33bd4c3b64044daee85843e08
Gustavo Takao <gustavo_hideo@hotmail.com>
2022-05-15 21:20:24 -0300

    fix searchBox

--- src/components/searchBox/index.jsx
+++ src/components/searchBox/index.jsx
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import { MainContainer, Container, TextContainer, Image, Input } from "./style";
 
-=======
 import { Container,TextContainer, Image, Input } from "./style";
 import React from "react";
->>>>>>> 752da9004938d37aaa56ea0f960942444dff8f1e
+
 
 const SearchBox = (props) => {
   return (
